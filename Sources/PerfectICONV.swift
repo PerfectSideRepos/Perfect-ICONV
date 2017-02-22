af89118d3a28318755d38f66260ea29ff2bdf231
Rockford Wei <rocky@perfect.org>
2017-02-22 17:03:27 -0500

    Adding Licence & Copyright Info

--- Sources/PerfectICONV.swift
+++ Sources/PerfectICONV.swift
@@ -1,3 +1,22 @@
+//
+//  PerfectICONV.swift
+//  Perfect-ICONV
+//
+//  Created by Rockford Wei on 2017-01-01.
+//  Copyright © 2017 PerfectlySoft. All rights reserved.
+//
+//===----------------------------------------------------------------------===//
+//
+// This source file is part of the Perfect.org open source project
+//
+// Copyright (c) 2017 - 2018 PerfectlySoft Inc. and the Perfect project authors
+// Licensed under Apache License v2.0
+//
+// See http://perfect.org/licensing.html for license information
+//
+//===----------------------------------------------------------------------===//
+//
+
 import iconv
 
 /// class Iconv
